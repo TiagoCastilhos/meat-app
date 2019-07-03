5d3ea271964985d4e1c9662e55e4d3a8a5166519
Tiago Henrique de Castilhos <tiagoxd13@hotmail.com>
2019-07-03 17:00:11 -0300

    first commit

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Restaurant } from "./restaurant/restaurant.model";
+import { MEAT_API } from '../app.api'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from 'app/app.errorHandler';
+
+@Injectable()
+export class RestaurantsService {
+    constructor(private http: Http) {
+    }   
+
+    getRestaurants() : Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(r => r.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    getRestaurant(id: string) : Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(r => r.json())
+            .catch(ErrorHandler.handleError)
+    }
+}
